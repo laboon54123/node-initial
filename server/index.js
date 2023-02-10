0eb33bfc7da2b8113723e54d6fb25d14ad8d0067
laboon <john.rodriguez.aza.92i@gmail.com>
2023-02-10 16:30:57 -0500

    version base de node

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { PORT } from "./config.js";
+
+import indexRoutes from './routes/index.routes.js'
+
+const app = express();
+
+app.use(indexRoutes)
+app.listen(PORT)
+
+console.log(`server running this port ${PORT}`)
