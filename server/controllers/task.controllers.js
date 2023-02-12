735d3f0cb2741a212d73e87fad2e2f7fdc359b24
laboon <john.rodriguez.aza.92i@gmail.com>
2023-02-12 17:24:42 -0500

    crud basico sin validaciones de error

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -1,20 +1,61 @@
-export const getTasks = function(req,res){
-    res.send('obteniendo tareas')
-}
+import { pool } from "../db.js";
 
-export const getTask = function(req,res){
-    res.send('obetniendo tarea')
-}
+export const getTasks = async function (req, res) {
+  const [result] = await pool.query(
+    "SELECT * FROM tasks ORDER BY createAt ASC"
+  );
+  console.log(result);
+  res.json(result);
+};
 
-export const createTask = function(req,res){
-    res.send('creando tareas')
-}
+export const getTask = async function (req, res) {
+  console.log(req.params.id);
+  const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
+    req.params.id,
+  ]);
 
-export const updateTask = function(req,res){
-    res.send('actualizando una tarea')
-}
+  if (result.length === 0)
+    return res.status(404).json({ message: "Task not found" });
 
-export const deleteTask = function(req,res){
-    res.send('eliminando tarea')
-}
+  res.json(result[0]);
+  console.log(`titulo de la tarea: ${result[0].title}`);
+};
 
+export const createTask = async function (req, res) {
+  const { title, description } = req.body;
+  const [result] = await pool.query(
+    "INSERT INTO tasks(title, description) VALUES(?,?)",
+    [title, description]
+  );
+  console.log(result);
+
+  res.json({
+    id: result.insertId,
+    status: result.serverStatus,
+    title,
+    description,
+  });
+};
+
+export const updateTask = async function (req, res) {
+  console.log(`parametro desde url(id):${req.params.id}`);
+
+  const result = await pool.query("UPDATE tasks SET ? WHERE id = ?", [
+    req.body,
+    req.params.id,
+  ]);
+  res.json(result);
+};
+
+export const deleteTask = async function (req, res) {
+  console.log(`parametro desde url(id):${req.params.id}`);
+
+  const [result] = await pool.query("DELETE FROM tasks WHERE id = ?", [
+    req.params.id,
+  ]);
+
+  if (result.affectedRows === 0)
+    return res.status(404).json({ message: "Task not found" });
+
+  return res.sendStatus(204);
+};
--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ import { PORT } from "./config.js";
 import indexRoutes from "./routes/index.routes.js";
 
 import taskRoutes from "./routes/task.routes.js";
+
 const app = express();
 
+app.use(express.json())
 
 app.use(indexRoutes);
 app.use(taskRoutes);
