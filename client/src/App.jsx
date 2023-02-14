a1dffb6821c79a087215147ac1e6f280a038e164
laboon <john.rodriguez.aza.92i@gmail.com>
2023-02-13 19:25:31 -0500

    react con router dom6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,20 @@
-function App(){
+import { Route, Routes } from "react-router-dom";
+import TasksPage from "./pages/TaskPage.jsx";
+import TaskForm from "./pages/TaskForm.jsx";
+
+import NotFound from "./pages/NotFound.jsx";
+
+import Navbar from "./components/navbar.jsx";
+function App() {
   return (
-    <div>hello world</div>
-  )
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<TasksPage />} />
+        <Route path="/new" element={<TaskForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
-export default App
+export default App;
---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <h1>React Mysql</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/new">Create Task</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
